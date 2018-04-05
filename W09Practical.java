16b425baef11e03d3bec0532feba5e9a979251f8
aindreboe <aindreboe@gmail.com>
2018-04-05 10:58:54 +0100

    hei hei

--- W09Practical.java
+++ W09Practical.java
@@ -1,4 +1,13 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -13,36 +22,71 @@ public class W09Practical {
 
         for (int i = 0; i < args.length; i++) { //THIS LOOKS DUMB
             if (args[i].startsWith("--")) {
-                switch (args[i]) {
+
+                String current = args[i];
+                i++;
+                String next = args[i];
+                System.out.println(current);
+                System.out.println(next);
+                switch (current) {
                     case "--search":
-                        if (args[i++].equals("author") ||
-                                args[i++].equals("venue")) {
-                            search = args[i++];
-                        } else if (args[i++].equals("publication")) {
+                        if (next.equals("author") ||
+                                next.equals("venue")) {
+                            search = next;
+                        } else if (next.equals("publication")) {
                             search = "publ";
                         } else {
-                            System.out.println("ERROR MESSAGE GOES HERE"); //FILL THIS IN
+                            System.out.println("ERROR MESSAGE GOES HERE 1"); //FILL THIS IN
                         }
                         break;
                     case "--query":
-                        query = args[i++];
+                        query = next;
                         break;
                     case "--cache":
-                        cache = args[i++];
+                        cache = next;
                         break;
                     default:
-                        System.out.println("ERROR MESSAGE GOES HERE"); //FILL THIS IN
+                        System.out.println("ERROR MESSAGE GOES HERE 2"); //FILL THIS IN
                 }
             }
         }
 
+        query = query.replaceAll(" ", "");
         String url = "http://dblp.org/search/" + search + "/api?q=" + query + "&format=xml&h=30&c=0";
+        System.out.println(url);
+
+        URL XMLurl;
 
         try {
-            URL XMLurl = new URL(url);
+            XMLurl = new URL(url);
+            System.out.println(XMLurl.toString());
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse(XMLurl.openStream());
+
+            NodeList nodeList = document.getElementsByTagName("author");
+            System.out.println(nodeList.getLength() + " nodes");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                Node node1 = node.getFirstChild();
+                //System.out.println(node1.getTextContent());
+                System.out.println(node.getTextContent());
+                //System.out.println(node.lookupPrefix());
+            }
+
         } catch (MalformedURLException e) {
             System.out.println("Error: " + e);
+        } catch (ParserConfigurationException e) {
+            System.out.println("Error: " + e);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        } catch (SAXException e) {
+            System.out.println("Error: " + e);
         }
+
 
         String urlEncode = URLEncoder.encode(url);
 
