7a512e6d92710550e1bdac5d9cf960c8799e9cdc
aindreboe <aindreboe@gmail.com>
2018-04-09 11:55:31 +0100

    Almost done

--- W09Practical.java
+++ W09Practical.java
@@ -22,6 +22,11 @@ public class W09Practical {
     private static String search;
     private static String query;
     private static String cache;
+    private static boolean searchB = false;
+    private static boolean queryB = false;
+    private static boolean cacheB = false;
+    private static File cacheFile;
+
 
     public static void main(String[] args) {
 
@@ -30,40 +35,83 @@ public class W09Practical {
 
                 String current = args[i];
                 i++;
-                String next = args[i];
+                //System.out.println(current);
+                //System.out.println(args[i]);
+                //String next = args[i];
                 switch (current) {
                     case "--search":
-                        if (next.equals("author") ||
-                                next.equals("venue")) {
-                            search = next;
-                        } else if (next.equals("publication")) {
-                            search = "publ";
+                        searchB = true;
+                        if (args.length <= i || args[i].startsWith("--") || args[i] == null || args[i].equals("")) {
+                            System.out.println("Missing value for " + current + "\nMalformed command line arguments.");
+                            System.exit(0);
                         } else {
-                            System.out.println("ERROR MESSAGE GOES HERE 1"); //FILL THIS IN
+                            if (args[i].equals("author") ||
+                                    args[i].equals("venue")) {
+                                search = args[i];
+                            } else if (args[i].equals("publication")) {
+                                search = "publ";
+                            } else {
+                                System.out.println("Invalid value for --search: " + args[i]); //FILL THIS IN
+                                System.exit(0);
+                            }
                         }
                         break;
                     case "--query":
-                        query = next;
+                        queryB = true;
+                        if (args.length <= i || args[i] == null || args[i].startsWith("--") || args[i].equals("") || args[i].equals(" ")) {
+                            System.out.println("Missing value for " + current + "\nMalformed command line arguments.");
+                            System.exit(0);
+                        } else {
+                            query = args[i];
+                        }
                         break;
                     case "--cache":
-                        cache = next;
+                        cacheB = true;
+                        if (args.length <= i) {
+                            cacheFile = new File(args[i]);
+                            if (!cacheFile.isDirectory()) {
+                                System.out.println("Cache directory doesn't exist: noSuchDirectory");
+                            }
+                        }
+                        if (args.length <= i || args[i] == null || args[i].startsWith("--") || args[i].equals("")) {
+                            System.out.println("Missing value for " + current + "\nMalformed command line arguments.");
+                            System.exit(0);
+                        } else {
+                            cache = args[i];
+                        }
                         break;
                     default:
                         System.out.println("ERROR MESSAGE GOES HERE 2"); //FILL THIS IN
+                        System.exit(0);
                 }
             }
         }
+        if (!queryB) {
+            System.out.println("Missing value for --query" + "\nMalformed command line arguments.");
+            System.exit(0);
+        }
+        if (!cacheB) {
+            System.out.println("Missing value for --cache" + "\nMalformed command line arguments.");
+            System.exit(0);
+        }
+        if (!searchB) {
+            System.out.println("Missing value for -search" + "\nMalformed command line arguments.");
+            System.exit(0);
+        }
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //query = query.replaceAll(" ", "");
+        query = query.replaceAll(" ", "+");
         String url = "http://dblp.org/search/" + search + "/api?q=" + query + "&format=xml&h=40&c=0";
+        //System.out.println(url);
         URL XMLurl;
 
         try {
 
             XMLurl = new URL(url);
 
-            String urlEncode = URLEncoder.encode(url);
+            String urlEncode = URLEncoder.encode(url) + ".xml";
+
+            //System.out.println(urlEncode);
 
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
@@ -72,21 +120,25 @@ public class W09Practical {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(document);
-            //StreamResult streamResult =  new StreamResult(new File(cache + "\\" + urlEncode));
-            //transformer.transform(source, streamResult);
+            StreamResult streamResult = new StreamResult(new File(cache + "\\" + urlEncode));
+            transformer.transform(source, streamResult);
 
             NodeList nodeList = null;
             NodeList nodeAuthor = null;
 
             switch (search) {
-                case "publ": nodeList = document.getElementsByTagName("title");
-                break;
-                case "venue": nodeList = document.getElementsByTagName("venue");
-                break;
-                case "author": nodeList = document.getElementsByTagName("author");
-                nodeAuthor = document.getElementsByTagName("url");
-                break;
-                default: return;
+                case "publ":
+                    nodeList = document.getElementsByTagName("info");
+                    break;
+                case "venue":
+                    nodeList = document.getElementsByTagName("venue");
+                    break;
+                case "author":
+                    nodeList = document.getElementsByTagName("author");
+                    nodeAuthor = document.getElementsByTagName("url");
+                    break;
+                default:
+                    return;
             }
 
             for (int i = 0, n = 0; i < nodeList.getLength(); i++, n++) {
@@ -100,7 +152,8 @@ public class W09Practical {
                     URL newURL = new URL(authorURL);
                     Document authorDocument = builder.parse(newURL.openStream());
 
-                    if (authorDocument == null) System.out.println("fuckity fuck fuck");
+                    if (authorDocument == null)
+                        System.out.println("fuckity fuck fuck");
 
                     //CHANGE URL, THE RESULTANT PAGE HAS A DIFFERENT URL
 
@@ -109,10 +162,20 @@ public class W09Practical {
 
                     Node art = articles.item(0);
 
-                    System.out.print(node.getTextContent() + " - " + articles.getLength() + " publications with " + coAuthors.getLength() + " co-authors. \n");
+                    System.out.println(node.getTextContent() + " - " + articles.getLength() + " publications with " + coAuthors.getLength() + " co-authors.");
 
                 } else {
-                    System.out.println(node.getTextContent());
+                    if (search.equals("publ")) {
+                        Node publication = nodeList.item(i).getFirstChild();
+                        NodeList authorList = publication.getChildNodes();
+                        int nAuthors = 0;
+                        if (authorList.item(0).getNodeName().equals("author")) {
+                            nAuthors = authorList.getLength();
+                        }
+                        System.out.println(node.getFirstChild().getNextSibling().getTextContent() + " (number of authors: " + nAuthors + ")");
+                    } else {
+                        System.out.println(node.getTextContent());
+                    }
                 }
             }
         } catch (MalformedURLException e) {
